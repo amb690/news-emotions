2e8338ca0b570ff65eba191bf955ee02af7c0e06
amb690 <albertombln@gmail.com>
2020-12-07 21:06:41 +0100

    Añade serial version id a ImportAllTickerResponse.

--- newsemotions/src/main/java/com/markettrender/newsemotions/models/responses/ImportAllTickersResponse.java
+++ newsemotions/src/main/java/com/markettrender/newsemotions/models/responses/ImportAllTickersResponse.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class ImportAllTickersResponse implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
 	private int createdTickers;
 	
 	private int updatedTickers;
