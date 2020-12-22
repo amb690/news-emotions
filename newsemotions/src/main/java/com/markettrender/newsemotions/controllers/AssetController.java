f3b0e68a144b77b35ac2a57bd71414e181715ea1
amb690 <albertombln@gmail.com>
2020-12-22 19:25:27 +0100

    Añade endpoint para obtener activo por símbolo. Añade test.

--- newsemotions/src/main/java/com/markettrender/newsemotions/controllers/AssetController.java
+++ newsemotions/src/main/java/com/markettrender/newsemotions/controllers/AssetController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import com.markettrender.newsemotions.models.entity.Asset;
 import com.markettrender.newsemotions.service.AssetService;
 
 @RestController
+@RequestMapping("/newsemotions/asset")
 public class AssetController {
 
 	@Autowired
---
+++ newsemotions/src/test/java/com/markettrender/newsemotions/controllers/AssetControllerTest.java
@@ -0,0 +1,35 @@
+package com.markettrender.newsemotions.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.markettrender.newsemotions.models.entity.Asset;
+import com.markettrender.newsemotions.service.AssetService;
+
+@WebMvcTest(controllers = AssetController.class)
+public class AssetControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private AssetService assetService;
+	
+	@Test
+	void getAssetByTickerTest() throws Exception {
+		
+		Mockito.when(assetService.findByTicker1(Mockito.anyString())).thenReturn(new Asset());
+		
+		this.mockMvc.perform(get("/newsemotions/asset/{ticker}", "GOLD", new Date()))
+			.andExpect(status().isOk());
+	}
+}
