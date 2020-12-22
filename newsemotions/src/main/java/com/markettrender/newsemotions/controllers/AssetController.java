924651494e7d7c143f6b3e62a8b1087af75b94d6
amb690 <albertombln@gmail.com>
2020-12-22 19:21:11 +0100

    Añade nuevo endpoint para obtener un activo por símbolo.

---
+++ newsemotions/src/main/java/com/markettrender/newsemotions/controllers/AssetController.java
@@ -0,0 +1,31 @@
+package com.markettrender.newsemotions.controllers;
+
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.markettrender.newsemotions.models.entity.Asset;
+import com.markettrender.newsemotions.service.AssetService;
+
+@RestController
+public class AssetController {
+
+	@Autowired
+	public AssetService assetService;
+	
+	@GetMapping("/{ticker}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<?> getAssetFromTicker(@PathVariable @NotBlank String ticker) throws Exception {
+
+		Asset response = assetService.findByTicker1(ticker);
+		
+		return new ResponseEntity<>(response, HttpStatus.OK);
+
+	}
+}
