66347fccb03795eb493c89d3876a3474400a55ce
amb690 <albertombln@gmail.com>
2021-01-31 19:37:52 +0100

    Añade origen cruzado a endpoints.

--- newsemotions/src/main/java/com/markettrender/newsemotions/controllers/DataCollectorController.java
+++ newsemotions/src/main/java/com/markettrender/newsemotions/controllers/DataCollectorController.java
@@ -8,9 +8,11 @@ import javax.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,7 @@ import com.markettrender.newsemotions.service.StockNewsDataCollectorService;
 
 @RestController
 @RequestMapping("/newsemotions/import")
+@CrossOrigin(origins = "*", methods= {RequestMethod.POST})
 public class DataCollectorController {
 	
 	@Autowired
--- newsemotions/src/main/java/com/markettrender/newsemotions/controllers/NewsEmotionsController.java
+++ newsemotions/src/main/java/com/markettrender/newsemotions/controllers/NewsEmotionsController.java
@@ -12,9 +12,11 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,7 @@ import com.markettrender.newsemotions.service.EmotionService;
 
 @RestController
 @RequestMapping("/newsemotions")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET})
 public class NewsEmotionsController {
 
 	@Autowired
